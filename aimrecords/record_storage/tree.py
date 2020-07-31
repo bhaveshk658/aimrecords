b897c173d12bc046b9e4184ee3310eaec678625a
Bhavesh Kalisetti <bhaveshk658@berkeley.edu>
2020-07-30 17:08:49 -0700

    Inserting and splitting nodes (basic)

--- aimrecords/record_storage/tree.py
+++ aimrecords/record_storage/tree.py
@@ -10,53 +10,32 @@ class Node:
     * List of pointers to other nodes
     - No add function/remove function
     """
-    def __init__(self, order, keys=None, children=None):
+    def __init__(self, order, keys=None, children=None, next=None, leaf=None):
         self.order = order
         self.keys = keys or []
         self.children = children or []
+        self.next = next
+        self.leaf = bool(leaf)
 
     def full(self):
         """
         Returns whether a node is full or not.
         """
-        return self.order == len(self.keys) + 1
-
-    def split(self):
+        return self.order == len(self.keys)
+    
+    def is_leaf(self):
         """
-        Split internal node into two nodes and move self
-        up as a parent.
+        Checks if leaf node.
         """
-        middle = self.order // 2
-        left = Node(self.order, self.keys[:middle],
-                    self.children[:middle])
-        right = Node(self.order, self.keys[middle:],
-                        self.children[middle:])
-
-        self.keys = [right.keys[0]]
-        self.children = [left, right]
+        return self.leaf
 
     def search(self, key):
         """
-        Binary search to see where an index should be inserted.
+        Binary search to see where a key should be inserted.
+        Also finds index of given key.
         """
         index = bisect_left(self.keys, key)
         return index
-        
-
-class Leaf(Node):
-    """
-    Leaf node object, order = n. Leaf nodes point to records.
-    Can have n children. Also contain additional sibling pointer.
-    * Order
-    * List of keys
-    * List of pointers to records
-    * Sibling pointer
-    """
-    def __init__(self, order, keys=None, children=None, next=None):
-        self.order = order
-        self.keys = keys or []
-        self.children = children or []
-        self.next = next
 
     def add(self, key, value):
         """
@@ -69,19 +48,20 @@ class Leaf(Node):
 
     def split(self):
         """
-        Split internal node into two nodes and move self
+        Split node into two nodes and move self
         up as a parent.
         """
         middle = self.order // 2
-        left = Leaf(self.order, self.keys[:middle],
-                    self.children[:middle])
-        right = Leaf(self.order, self.keys[middle:],
-                        self.children[middle:])
+        left = Node(self.order, self.keys[:middle],
+                    self.children[:middle], leaf=self.leaf)
+        right = Node(self.order, self.keys[middle:],
+                        self.children[middle:], leaf=self.leaf)
 
+        self.leaf = False
         self.keys = [right.keys[0]]
         self.children = [left, right]
-        left.next = right
-
+        if left.is_leaf() and right.is_leaf():
+            left.next = right
 
 class BPTree():
     """
@@ -92,10 +72,53 @@ class BPTree():
     - search, insert, delete
     """
     def __init__(self, order):
-        self.root = Leaf(order)
+        self.root = Node(order, leaf=True)
+
+    def merge_nodes(self, parent, child, index):
+        """
+        Merge a child node and parent node. Occurs when a node
+        splits and the parent node isn't full.
+        """
+        parent.children.pop(index)
+        pivot = child.keys[0]
+
+        for i, item in enumerate(parent.keys):
+            if pivot < item:
+                parent.keys = parent.keys[:i] + [pivot] + parent.keys[i:]
+                parent.children = parent.children[:i] + child.children + parent.children[i:]
+                break
+
+            elif i + 1 == len(parent.keys):
+                parent.keys += [pivot]
+                parent.children += child.children
+                break
 
     def insert(self, key, value):
-        return
+        """
+        Insert a key-value pair into the tree.
+        """
+        parent = None
+        child = self.root
+
+        while not child.is_leaf():
+            parent = child
+            index = child.search(key)
+            child = child.children[index]
+
+        child.add(key, value)
+        
+        if child.full():
+            child.split()
+
+            if parent:
+                while parent.full():
+                    
 
     def __getitem__(self, key):
-        return ""
+        node = self.root
+        
+        while not node.is_leaf():
+            index = node.search(key)
+            node = node.children[index]
+        
+        return node.children[node.search(key)]
